910df79ef2012d380f42aa4a575c89f0ae1e9858
Ceberus132 <ceberus132@gmail.com>
2024-11-13 23:23:20 +0100

    feat: added initial pong response

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import { Router } from 'itty-router';
-import { verifyKey } from 'discord-interactions';
+import { Router, json } from 'itty-router';
+import { InteractionResponseType, InteractionType, verifyKey } from 'discord-interactions';
 
 export interface Env {
 	DISCORD_TOKEN: string;
@@ -17,13 +17,23 @@ router.get('/', async () => {
 	return new Response('Success');
 });
 
+// default route for all requests sent from Discord. JSON payload described here: https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object
+router.post('/', async (request: Request, env: Env) => {
+	const { isValid, interaction } = await verifyRequest(request, env);
+	if (!isValid) return new Response('Bad request signature.', { status: 401 });
+
+	if (interaction.type === InteractionType.PING) {
+		return json({ type: InteractionResponseType.PONG });
+	}
+});
+
 // function to verify the discord request
 async function verifyRequest(request: Request, env: Env) {
 	const signature = request.headers.get('x-signature-ed25519');
 	const timestamp = request.headers.get('x-signature-timestamp');
 	const body = await request.text();
 	// check for validity and send the interaction
-	const isValid = signature && timestamp && verifyKey(body, signature, timestamp, env.DISCORD_PUBLIC_KEY);
+	const isValid = signature && timestamp && await verifyKey(body, signature, timestamp, env.DISCORD_PUBLIC_KEY);
 	return { isValid, interaction: isValid ? JSON.parse(body) : null };
 }
 
