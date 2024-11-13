bf921ed8b29620839cd867a87f5f23d51d8cafe9
Ceberus132 <ceberus132@gmail.com>
2024-11-13 23:51:45 +0100

    chore: small improvements

--- src/lib/randomRock.ts
+++ src/lib/randomRock.ts
@@ -6,8 +6,8 @@ export async function getRock(env: Env) {
 	// get image from r2 bucket
 	const object = await env.FancyRockImages.get(`rock${imageNumber}.jpg`)
 	if (!object) {
-		// TODO add extra e-mail
-		throw new Error('Could not find a rock image, please try again :d \n\n If this issue persists please contact me: [Coming soon™]')
+    // TODO add extra e-mail
+    throw new Error('Could not find a rock image, please try again :d \n\n If this issue persists please contact me: [Coming soon™]')
 	}
 	const image = new File([await object.blob()], "rock.jpg", {type: 'image/jpg'})
 	return { imageNumber, image }
