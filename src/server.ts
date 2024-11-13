f7e2e966d01b850f6f7af2928890dd0870e8ef13
Ceberus132 <ceberus132@gmail.com>
2024-11-13 23:16:40 +0100

    feat: added function to verify the discord request

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { Router } from 'itty-router';
+import { verifyKey } from 'discord-interactions';
 
 export interface Env {
 	DISCORD_TOKEN: string;
@@ -15,6 +16,16 @@ const router = Router();
 router.get('/', async () => {
 	return new Response('Success');
 });
+
+// function to verify the discord request
+async function verifyRequest(request: Request, env: Env) {
+	const signature = request.headers.get('x-signature-ed25519');
+	const timestamp = request.headers.get('x-signature-timestamp');
+	const body = await request.text();
+	// check for validity and send the interaction
+	const isValid = signature && timestamp && verifyKey(body, signature, timestamp, env.DISCORD_PUBLIC_KEY);
+	return { isValid, interaction: isValid ? JSON.parse(body) : null };
+}
 
 // export server
 const server = {
