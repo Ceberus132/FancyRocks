667a401a762e4a81819f2535c0672750b0c6a4bd
Ceberus132 <ceberus132@gmail.com>
2024-11-13 23:50:30 +0100

    feat: added helper function to get an image of a rock

---
+++ src/lib/randomRock.ts
@@ -0,0 +1,14 @@
+import { Env } from '../server';
+
+export async function getRock(env: Env) {
+	const amount = await env.FancyRockImages.list();
+	const imageNumber = Math.floor(Math.random() * amount.objects.length);
+	// get image from r2 bucket
+	const object = await env.FancyRockImages.get(`rock${imageNumber}.jpg`)
+	if (!object) {
+		// TODO add extra e-mail
+		throw new Error('Could not find a rock image, please try again :d \n\n If this issue persists please contact me: [Coming soon™]')
+	}
+	const image = new File([await object.blob()], "rock.jpg", {type: 'image/jpg'})
+	return { imageNumber, image }
+}
