4d96374559a7e5a4a743992c413f9c710a666579
Ceberus132 <ceberus132@gmail.com>
2024-11-13 16:38:51 +0100

    feat: added initial router

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,17 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Router } from 'itty-router';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+export interface Env {
+	DISCORD_TOKEN: string;
+	DISCORD_PUBLIC_KEY: string;
+	DISCORD_APPLICATION_ID: string;
+	VERSION: string;
+	FancyRockImages: R2Bucket;
+}
+
+// init itty-router
+const router = Router();
+
+// simple response to verify that the worker bot is running
+router.get('/', async (request: Request) => {
+	return new Response('Success');
+});
