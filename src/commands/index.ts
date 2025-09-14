e3d5e9e3010fecd03a208031d1b906ee361c05dc
Ceberus132 <ceberus132@gmail.com>
2025-09-14 11:40:51 +0200

    feat: added /invite command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,5 +2,6 @@ import { fancyrock } from '@commands/fancyrock';
 import { givefancyrock } from '@commands/givefancyrock';
 import { throwfancyrock } from '@commands/throwfancyrock';
 import { about } from '@commands/about';
+import { invite } from '@commands/invite';
 
-export default [fancyrock, givefancyrock, throwfancyrock, about];
+export default [fancyrock, givefancyrock, throwfancyrock, about, invite];
